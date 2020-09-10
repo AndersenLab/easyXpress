40271337ca94b5b3610c2855e09b915892381a0f
Tim Crombie <tim.crombie@gmail.com>
2020-09-10 14:35:46 -0500

    add guess_max = 50000 for design classes

--- R/readXpress.R
+++ R/readXpress.R
@@ -89,7 +89,7 @@ readXpress <- function(filedir, design = FALSE, rdafile = NULL) {
     #make a design file list
     design_file_list <- list.files(glue::glue("{filedir}/design"))
     #join design file to raw_data. na.omit will remove rows with any NAs
-    design_file <- readr::read_csv(glue::glue("{filedir}/design/{design_file_list[1]}")) %>%
+    design_file <- readr::read_csv(glue::glue("{filedir}/design/{design_file_list[1]}"), guess_max = 50000) %>%
       dplyr::mutate(Metadata_Plate = dplyr::case_when(
         Metadata_Plate == 1 ~ "01",
         Metadata_Plate == 2 ~ "02",
